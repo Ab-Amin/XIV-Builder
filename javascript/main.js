322669d93df043c9fc7ebe9e385d2b6b174d455a
Ab-Amin <abourrabiaamin@gmail.com>
2024-05-16 04:26:31 +0200

    added popup window for futur gear-selector inside

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,4 @@
-// fetch(`https://api.rawg.io/api/games?key=09715d2ec4fa4c6cb8fc1d6196daaa30`)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {console.log("Erreur lors de la récup des données :", error);
-// })
-
-// private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0
+// xivapi private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0
 
 // fetch("https://xivapi.com/item/1675?columns=ID,Name,Description,Lv,LevelItem,ClassJobCategory.Name&private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0", { mode: 'cors' })
 // 	.then(response => response.json())
@@ -16,37 +8,40 @@
 //   .catch(error => {console.log("Erreur lors de la récup des données :", error);
 // })
 
-fetch("https://xivapi.com/character/search?name=Nima+Min&server=Twintania&private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0", { mode: 'cors' })
-	.then(response => response.json())
-	.then(data => {
-    console.log(console.info(data));
-  })
-  .catch(error => {console.log("Erreur lors de la récup des données :", error);
-})
+// =-=-=-=-=| XIVAPI : charachters |=-=-=-=-=
 
-fetch("https://xivapi.com/character/36077235&private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0", { mode: 'cors' })
-	.then(response => response.json())
-	.then(data => {
-    console.log(console.info(data));
-  })
-  .catch(error => {console.log("Erreur lors de la récup des données :", error);
-})
+// fetch("https://xivapi.com/character/search?name=Nima+Min&server=Twintania&private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0", { mode: 'cors' })
+// 	.then(response => response.json())
+// 	.then(data => {
+//     console.log(console.info(data));
+//   })
+//   .catch(error => {console.log("Erreur lors de la récup des données :", error);
+// })
+
+// fetch("https://xivapi.com/character/36077235&private_key=0be9dad833724e1ebab95b987f3c0166155d12ff1a2f4adc9ee0d7a5ddd9c4d0", { mode: 'cors' })
+// 	.then(response => response.json())
+// 	.then(data => {
+//     console.log(console.info(data));
+//   })
+//   .catch(error => {console.log("Erreur lors de la récup des données :", error);
+// })
 
 
-// =-=-=| Variables :
+// =-=-=-=-=| Variables |=-=-=-=-=
 const itemId = 0;
 const SingItemName = 10;
 const itemLevel = 12;
 const levelReq = 41;
 
-// todo : translate icon code into images
+// todo : translate icon codes into images with xiv icon api
 const icon = 11;
 
-// todo : gear category => translate num into class name 
+// https://docs.google.com/spreadsheets/d/1KtWCELA7ggvYXYumxIib-n3ZbD1UKlnaXyQiy-6LiTI/edit?usp=sharing
+// todo : gear category => translate num into gear type category + num into class names 
 
 
 
-// =-=-=| CSV fetch :
+// =-=-=-=-=| CSV fetch |=-=-=-=-=
 fetch('https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item.csv')
   .then(response => response.text())
   .then(csvData => {
@@ -107,10 +102,40 @@ fetch('https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item
 .catch(err => console.error('Error fetching CSV:', err));
 
 
+// =-=-=-=-=| Gear window popup |=-=-=-=-=
+// todo : animation when popup closes
+
+let gearGrid = document.querySelector('.gear-grid')
+let gearWindow = document.querySelector('.search-window')
+let gearWindowX = document.querySelector('.search-window--x')
 
 
+function openGearWindow() {
+  gearWindow.classList.add('gear-window-pop')
+  console.log('opening popup');
+}
+function closeGearWindow() {
+  gearWindow.classList.remove('gear-window-pop')
+  console.log('closing popup');
+}
 
+gearGrid.addEventListener('click', e => {
+  
+  if (e.target.classList.contains('gear-box')){
+    // Stop event propagation to prevent the document click listener from being immediately triggered
+    e.stopPropagation();
+    openGearWindow()
+  }
+})
 
+gearWindowX.addEventListener('click', closeGearWindow)
+
+document.addEventListener('click', function(e) {
+  // Close the popup if the click is outside of it
+  if (!gearWindow.contains(e.target) && e.target !== gearWindowX) {
+    closeGearWindow()
+  }
+});
 
 
 
